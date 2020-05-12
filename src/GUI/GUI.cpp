81225451963d2a91dee2f3fd9af6163257b6d989
Michael <michael_kirsch>
2020-05-12 09:49:38 +0200

    working gui template with hooks

--- src/GUI/GUI.cpp
+++ src/GUI/GUI.cpp
@@ -9,11 +9,3 @@ void MBGL::GUI::GUI::init() {
 void MBGL::GUI::GUI::update() {
 
 }
-
-void MBGL::GUI::GUI::render() {
-
-}
-
-void MBGL::GUI::Unit::render() {
-
-}
--- src/GUI/GUI.h
+++ src/GUI/GUI.h
@@ -8,11 +8,16 @@
 #include "SFML/Graphics.hpp"
 #include "WindowManager.h"
 #include <memory>
+#include <iostream>
+#include <list>
 namespace MBGL{
     namespace GUI {
 
         struct Outline
         {
+            Outline(){};
+            Outline(float i_x,float i_y,float i_width,float i_height):x(i_x),y(i_y),width(i_width),height(i_height){};
+            Outline(glm::vec4 input):x(input.x),y(input.y),width(input.z),height(input.w){};
             float x=0.f;
             float y=0.f;
             float width=0.f;
@@ -23,52 +28,96 @@ namespace MBGL{
         {
         public:
             RenderingUnit()=default;
+            void render_test(std::string mes){
+                std::cout << "Rendering Test " << mes << std::endl;
+            }
         };
 
         struct Widget
         {
-            virtual void render()=0;
+            virtual void render(RenderingUnit* r_unit)=0;
             RenderingUnit* r_unit;
+            Outline m_outline;
         };
 
-        class Hook
+        struct Hook
         {
-            void hook(Widget* to_hook){children.emplace_back(to_hook);};
-            std::vector<Widget*> children;
+            void hook(Widget* to_hook){
+                    std::cout << "HOOK " << std::endl;
+                    children.push_back(to_hook);
+                };
+            void unhook(Widget* to_unhook)
+            {
+                std::cout << "UNHOOK " << std::endl;
+                children.remove(to_unhook);
+            }
+            std::list<Widget*> children;
         };
 
-        class GUI : public GameObject,Hook //GUI will be the base class.
+        class GUI : public GameObject,public Hook //GUI will be the base class.
         {
         public:
-            GUI(WindowManager& mgr);
-            ~GUI();
+            GUI(WindowManager& mgr){};
+            ~GUI(){};
             void init();
 
             void update();
 
-            void render();
+            void render(){
+                std::cout << "GUI MAIN RENDER " << std::endl;
+                for(auto& ch:children)
+                    ch->render(&r_unit);
+            };
+        private:
+            RenderingUnit r_unit;
         };
 
-        class Unit : public Widget,Hook
+        class Unit : public Widget,public Hook
         {
         public:
-            Unit(GUI* parent);
-            Unit(Unit* parent,Outline outline);
-            void render(RenderingUnit* r_unit);
+            Unit(GUI* parent, Outline outline){
+                m_outline = outline;
+                parent->hook(this);};
+            Unit(Unit* parent,Outline outline){
+                parent->hook(this);
+                m_outline=outline;
+                m_parent = parent;
+            };
+            void render(RenderingUnit* r_unit){
+                r_unit->render_test("Unit");
+                for(auto& ch:children)
+                    ch->render(r_unit);
+            };
+            ~Unit()
+            {
+                if(m_parent!= nullptr)
+                    m_parent->unhook(this);
+            };
+        private:
+            Unit* m_parent = nullptr;
         };
 
         class Button : public Widget
         {
         public:
-            Button(Unit* parent, Outline outline);
+            Button(Unit* parent, Outline outline)
+            {
+                m_outline=outline;
+                parent->hook(this);
+                m_parent=parent;
+            };
+            void render(RenderingUnit *r_unit){
+                r_unit->render_test("Button");
+            };
+            ~Button()
+            {
+                if(m_parent!= nullptr)
+                    m_parent->unhook(this);
+            };
+
+        private:
+            Unit* m_parent = nullptr;
         };
-
-        class List : public Unit
-        {
-        public:
-
-        };
-
 
     }
 }
