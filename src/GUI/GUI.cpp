7b26d0ba8e2190894ac70048854ef661e327c7b8
Michael <michael_kirsch>
2020-05-12 18:16:03 +0200

    colorpalett can now be loaded from a file

--- src/GUI/GUI.cpp
+++ src/GUI/GUI.cpp
@@ -1,11 +1,3 @@
 
 
 #include "GUI.h"
-
-void MBGL::GUI::GUI::init() {
-
-}
-
-void MBGL::GUI::GUI::update() {
-
-}
--- src/GUI/GUI.h
+++ src/GUI/GUI.h
@@ -10,129 +10,185 @@
 #include <memory>
 #include <iostream>
 #include <list>
-namespace MBGL{
+#include <fstream>
+
+namespace MBGL {
     namespace GUI {
 
-        struct Outline
-        {
-            Outline(){};
-            Outline(float i_x,float i_y,float i_width,float i_height):x(i_x),y(i_y),width(i_width),height(i_height){};
-            Outline(glm::vec4 input):x(input.x),y(input.y),width(input.z),height(input.w){};
-            float x=0.f;
-            float y=0.f;
-            float width=0.f;
-            float height=0.f;
+        struct ColorPalette {
+            enum ColorType {
+                Background,
+                SecondaryBackground,
+                Widget_Primary,
+                Widget_Secondary,
+                Success,
+                Warning,
+                Error,
+                Text_Primary,
+                Text_Inactive
+            };
+
+            ColorPalette() {};
+
+            ColorPalette(std::vector<glm::vec3> i_colors) : colors(i_colors) {};
+
+            ColorPalette(std::string path_to_file) {
+                loadFromFile(path_to_file);
+            };
+
+            void loadFromFile(std::string path) {
+                //load all the colors from a specified file
+                std::ifstream file;
+                file.open(path);
+                if (file.is_open()) {
+                    std::string line;
+                    while (std::getline(file, line)) {
+
+                        colors.emplace_back(getRGBoutOfString(line.substr(0,6)));
+                    }
+                    file.close();
+                }
+            }
+
+            glm::vec3 getColor(ColorType type) { return colors[type]; };
+        private:
+            glm::vec3 getRGBoutOfString(std::string colorString) {
+                float r = (1.f / 255.f) * std::stoi(colorString.substr(0, 2), nullptr, 16);
+                float g = (1.f / 255.f) * std::stoi(colorString.substr(2, 2), nullptr, 16);
+                float b = (1.f / 255.f) * std::stoi(colorString.substr(4, 2), nullptr, 16);
+                return {r, g, b};
+            }
+
+            std::vector<glm::vec3> colors;
         };
 
-        class RectangleUnit
-        {
+        struct Outline {
+            Outline() {};
+
+            Outline(float i_x, float i_y, float i_width, float i_height) : x(i_x), y(i_y), width(i_width),
+                                                                           height(i_height) {};
+
+            Outline(glm::vec4 input) : x(input.x), y(input.y), width(input.z), height(input.w) {};
+            float x = 0.f;
+            float y = 0.f;
+            float width = 0.f;
+            float height = 0.f;
+        };
+
+        class RectangleUnit {
             RectangleUnit(std::string vertex_shader_path, std::string fragment_shader_path);
+
             std::vector<float> vbo_data;
         };
 
 
-        class RenderingUnit
-        {
+        class RenderingUnit {
         public:
-            RenderingUnit()=default;
-            void add_data(std::string mes){
+            RenderingUnit() = default;
+
+            void add_data(std::string mes) {
                 std::cout << "Rendering Test " << mes << std::endl;
             }
-            void display()
-            {
-              std::cout << "FINAL DISPLAY OF GUI" << std::endl;
+
+            void display() {
+                std::cout << "FINAL DISPLAY OF GUI" << std::endl;
             };
         };
 
-        struct Widget
-        {
-            virtual void render(RenderingUnit* r_unit)=0;
-            RenderingUnit* r_unit;
+        struct Widget {
+            virtual void render(RenderingUnit *r_unit) = 0;
+
+            RenderingUnit *r_unit;
             Outline m_outline;
+            glm::vec3 m_color = {0.858, 0, 0.823};
         };
 
-        struct Hook
-        {
-            void hook(Widget* to_hook){
-                    std::cout << "HOOK " << std::endl;
-                    children.push_back(to_hook);
-                };
-            void unhook(Widget* to_unhook)
-            {
+        struct Hook {
+            void hook(Widget *to_hook) {
+                std::cout << "HOOK " << std::endl;
+                children.push_back(to_hook);
+            };
+
+            void unhook(Widget *to_unhook) {
                 std::cout << "UNHOOK " << std::endl;
                 children.remove(to_unhook);
             }
-            std::list<Widget*> children;
+
+            std::list<Widget *> children;
         };
 
-        class GUI : public GameObject,public Hook //GUI will be the base class.
+        class GUI : public GameObject, public Hook //GUI will be the base class.
         {
         public:
-            GUI(WindowManager& mgr){
-                auto& window = mgr.getWindow();
+            GUI(WindowManager &mgr) {
+                auto &window = mgr.getWindow();
 
             };
-            ~GUI(){};
-            void init();
 
-            void update();
+            ~GUI() {};
 
-            void render(){
+            void init() {};
+
+            void update() {};
+
+            void render() {
                 std::cout << "GUI MAIN RENDER " << std::endl;
-                for(auto& ch:children)
+                for (auto &ch:children)
                     ch->render(&r_unit);
                 r_unit.display();
             };
             Outline main_outline;
+            ColorPalette colorPalette;
         private:
             RenderingUnit r_unit;
         };
 
-        class Unit : public Widget,public Hook
-        {
+        class Unit : public Widget, public Hook {
         public:
-            Unit(GUI* parent, Outline outline){
+            Unit(GUI *parent, Outline outline) {
                 m_outline = outline;
-                parent->hook(this);};
-            Unit(Unit* parent,Outline outline){
                 parent->hook(this);
-                m_outline=outline;
+            };
+
+            Unit(Unit *parent, Outline outline) {
+                parent->hook(this);
+                m_outline = outline;
                 m_parent = parent;
             };
-            void render(RenderingUnit* r_unit){
+
+            void render(RenderingUnit *r_unit) {
                 r_unit->add_data("Unit");
-                for(auto& ch:children)
+                for (auto &ch:children)
                     ch->render(r_unit);
             };
-            ~Unit()
-            {
-                if(m_parent!= nullptr)
+
+            ~Unit() {
+                if (m_parent != nullptr)
                     m_parent->unhook(this);
             };
         private:
-            Unit* m_parent = nullptr;
+            Unit *m_parent = nullptr;
         };
 
-        class Button : public Widget
-        {
+        class Button : public Widget {
         public:
-            Button(Unit* parent, Outline outline)
-            {
-                m_outline=outline;
+            Button(Unit *parent, Outline outline) {
+                m_outline = outline;
                 parent->hook(this);
-                m_parent=parent;
+                m_parent = parent;
             };
-            void render(RenderingUnit *r_unit){
+
+            void render(RenderingUnit *r_unit) {
                 r_unit->add_data("Button");
             };
-            ~Button()
-            {
-                if(m_parent!= nullptr)
+
+            ~Button() {
+                if (m_parent != nullptr)
                     m_parent->unhook(this);
             };
 
         private:
-            Unit* m_parent = nullptr;
+            Unit *m_parent = nullptr;
         };
 
     }
